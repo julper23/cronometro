fbca52ba21ffb949372788cc4f26186ba32c58c8
jperezcoca <jperezcoca@ceit.es>
2023-07-18 19:05:44 +0200

    Creado hoock cronometro

---
+++ src/hooks/useCronometro.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+
+export default function useCronometro() {
+    const [diff, setDiff] = useState<any>(null)
+    const [initial, setInitial] = useState<any>(null)
+    const [pause,setPause] = useState<boolean>(false)
+    const [stop,setStop] = useState<boolean>(false)
+
+    const tick = () =>{
+        setDiff(new Date(+new Date() - initial))
+    }
+
+    useEffect(() => {
+        if(initial&&!pause&&!stop){
+            requestAnimationFrame(tick);
+        }
+        if(stop){
+            setDiff(null)
+            setInitial(null)
+        }
+    },[initial])
+
+    useEffect(() => {
+        if(diff&&!pause&&!stop){
+            requestAnimationFrame(tick);
+        }
+        if(stop){
+            setDiff(null)
+            setInitial(null)
+        }
+
+    },[diff])
+
+    useEffect(() => {
+        if(stop){
+            setDiff(null)
+            setInitial(null)
+        }
+
+
+    },[stop])
+
+    const startC = () => {
+        setPause(false)
+        setStop(false)
+        setInitial(+new Date())
+    }
+    const pauseC = () => {
+        if(diff){
+            if(pause){
+                requestAnimationFrame(tick);
+                
+            }
+            setPause(!pause)
+        }
+    }
+    const stopC = () => {
+        if(diff){
+            setStop(true)
+            setPause(false)
+        }
+    }
+
+    const timeFormat = (tm:any) => {
+        
+        if(!tm) return "00:00:00:00"
+        let hh = tm.getUTCHours()
+        let mm = tm.getUTCMinutes()
+        let ss = tm.getSeconds()
+        let cm:any = Math.round(tm.getMilliseconds()/10)
+      
+        hh = hh < 10 ? "0"+hh : hh
+        mm = mm < 10 ? "0"+mm : mm
+        ss = ss < 10 ? "0"+ss : ss
+        cm = cm < 10 ? "0"+cm : cm
+      
+        return `${hh}:${mm}:${ss}:${cm}`
+    }
+
+    return {startC,pauseC,stopC,timeFormat,pause,diff}
+}
